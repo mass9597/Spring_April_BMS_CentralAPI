93d279de55e8751844c1e7fdf2ccafce001173f3
Vignesh <vvignesh10399@gmail.com>
2025-04-19 22:34:41 +0530

    integ apis

--- central_api_v1/src/main/java/com/bms/central_api_v1/integration/DbApi.java
+++ central_api_v1/src/main/java/com/bms/central_api_v1/integration/DbApi.java
@@ -5,7 +5,10 @@ import com.bms.central_api_v1.requestdto.CreateUserDb;
 import com.bms.central_api_v1.util.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
@@ -17,7 +20,7 @@ public class DbApi {
     @Autowired
     Mapper mapper;
 
-    public void CreateUser(CreateUserDb createUserDb){
+    public AppUser CreateUser(CreateUserDb createUserDb){
 
         AppUser user = mapper.mapCreateUserDbToAppUser(createUserDb); // this will convert the requestdto to appuser class
 
@@ -25,5 +28,13 @@ public class DbApi {
         URI finalUrl = URI.create(url);
 
         RequestEntity request = RequestEntity.post(finalUrl).body(user);
+
+        //TO HIT the endpoint we need resttemplate library
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<AppUser> response = restTemplate.exchange(finalUrl, HttpMethod.POST,request, AppUser.class);
+
+        return response.getBody();
     }
 }
